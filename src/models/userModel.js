762cb07488677d4b0e07c52e44446c45bafc7906
ascebiswajit <ascebiswajit@gmail.com>
2025-07-27 20:26:52 +0530

    formated the model file

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,35 @@
-import pool from '../config/db.js';
+import pool from "../config/db.js";
 
 export const getAllUsersService = async () => {
-    const result = await pool.query('SELECT * FROM users');
-    return result.rows;
-}
+  const result = await pool.query("SELECT * FROM users");
+  return result.rows;
+};
 
 export const getUserByIdService = async (id) => {
-    const result  = await pool.query('SELECT * FROM users Where id = $1', [id]);
-    return result.rows[0];
-
-}
-export const createUserService = async (name,email) => {
-    const result = await pool.query('Insert into users (name,email) values ($1,$2) RETURNING *', [name, email]);
-    return result.rows[0];
-    
-}
-export const updateUserService = async (id,name,email) => {
-
-    const result = await pool.query( 'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name,email,id]);
-    return result.rows[0];
-
-}
+  const result = await pool.query("SELECT * FROM users Where id = $1", [id]);
+  return result.rows[0];
+};
+export const createUserService = async (name, email) => {
+  const result = await pool.query(
+    "Insert into users (name,email) values ($1,$2) RETURNING *",
+    [name, email]
+  );
+  return result.rows[0];
+};
+export const updateUserService = async (id, name, email) => {
+  const result = await pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+    [name, email, id]
+  );
+  return result.rows[0];
+};
 export const deleteUserService = async (id) => {
- const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
-    if (result.rowCount === 0) {
-        throw new Error('User not found');
-    }
-    return result.rows[0];
-}
-
+  const result = await pool.query(
+    "DELETE FROM users WHERE id = $1 RETURNING *",
+    [id]
+  );
+  if (result.rowCount === 0) {
+    throw new Error("User not found");
+  }
+  return result.rows[0];
+};
