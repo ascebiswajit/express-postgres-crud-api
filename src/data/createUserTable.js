0df6e1f0fee88130597271ae960ab19a8ee4ec2e
ascebiswajit <ascebiswajit@gmail.com>
2025-07-27 20:09:24 +0530

    Add password column to USERS table schema

--- src/data/createUserTable.js
+++ src/data/createUserTable.js
@@ -1,12 +1,12 @@
 import pool from '../config/db.js';
 
 const createUserTable = async () => {
-
     const queryText = `
     CREATE TABLE IF NOT EXISTS USERS(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
     )
